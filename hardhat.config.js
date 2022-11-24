4cef59e2fdded03355d407d7c04d681cfe4642b2
unknown <ubavictor24@gmail.com>
2022-11-24 01:04:51 +0100

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+module.exports = {
+  solidity: "0.8.17",
+  networks : {
+    goerli:{
+      url: process.env.NODE_URL,
+      accounts: [process.env.PRIV_KEY],
+      chainId: 5,
+    }
+  }
+}
+
