c65688aa4b863f7c612d0b174d28be50b734d202
unknown <ubavictor24@gmail.com>
2022-12-04 19:19:32 +0100

    latest changed

--- src/components/mint/NftInfor.jsx
+++ src/components/mint/NftInfor.jsx
@@ -11,89 +11,89 @@ const fileTypes = ["MP4", "MP3", "JPG", "WAV"];
 var contractAddress = "0x1d0D0cd9Be01fE529896e61FaE08a1662D368A6D"; // for testing
 
 export default function NftInformation() {
-  const [wallet, setWallet] = useState("");
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [status, setStatus] = useState("");
-  const [category, setCategory] = useState("");
-  const [price, setPrice] = useState("");
-  const [selectedFile, setSelectedFile] = useState();
-  const [title, setTitle] = useState("");
-  const [external_url, setExternalURL] = useState("");
+      const [wallet, setWallet] = useState("");
+      const [name, setName] = useState("");
+      const [description, setDescription] = useState("");
+      const [status, setStatus] = useState("");
+      const [category, setCategory] = useState("");
+      const [price, setPrice] = useState("");
+      const [selectedFile, setSelectedFile] = useState();
+      const [title, setTitle] = useState("");
+      const [external_url, setExternalURL] = useState("");
 
-  const changeHandler = (file) => {
-    setSelectedFile(file);
-  };
+      const changeHandler = (file) => {
+        setSelectedFile(file);
+      };
 
-  const MintNFT = async () => {
-    if (!name || !description || !price || !selectedFile || !title) {
-      alert("Incomplete Field");
-      return;
-    }
-
-    const formData = new FormData();
-
-    formData.append("file", selectedFile);
-
-    const metadata = JSON.stringify({
-      name: category,
-    });
-    formData.append("pinataMetadata", metadata);
-
-    const options = JSON.stringify({
-      cidVersion: 0,
-    });
-    formData.append("pinataOptions", options);
-
-    try {
-      const res = await fileUpload(
-        formData,
-        name,
-        title,
-        description,
-        external_url
-      );
-
-      if (res.success === true) {
-        try {
-          await ConnectToMetamask();
-
-          var metadataURL = res.pinataURL;
-          console.log(metadataURL);
-          const provider = new ethers.providers.Web3Provider(window.ethereum);
-          const signer = provider.getSigner();
-
-          // Contract call
-          let contract = new ethers.Contract(contractAddress, abi, signer);
-          ethers.utils.parseEther(price);
-
-          // let listingPrice = await contract.getListPrice();
-          // listingPrice = listingPrice.toString();
-
-          //actually create the NFT
-          let tip = { value: ethers.utils.parseEther("0.1") };
-
-          //  var url1 = "https://gateway.pinata.cloud/ipfs/QmcZKH8Vu5D8CAkANQg5ocHmFsmzDqVQXh2efM6NRcCgoQ"
-          //var url2 = https://gateway.pinata.cloud/ipfs/QmUSGVFeHLcEp6EFuLT1CSbHGQgRkrdMxciAPghA7nvhyM
-          // var url3 https://gateway.pinata.cloud/ipfs/QmdCRNUsGERLXBy4mdhQajqVBaR45dCPGP2ihymokWWGkh
-          var url4 =
-            "https://gateway.pinata.cloud/ipfs/QmSA2Q9RaqwDf4MWdZ1xNYyn7PFxajpiVWDrhfNCLSZ2zz";
-          let transaction = await contract.createToken(metadataURL, price, tip);
-
-          await transaction.wait();
-          // setStatus("NFT Minted Successfully")
-          alert("transaction Succesful");
-
-          window.location.replace("/profile");
-        } catch (err) {
-          console.log(err.message);
+      const MintNFT = async () => {
+        if (!name || !description || !price || !selectedFile || !title) {
+          alert("Incomplete Field");
+          return;
         }
-      } else {
-        setStatus("NFT Upload to PINNATA UNSUCCESSFUL");
-      }
-    } catch (err) {
-      console.log(err.message);
-    }
+
+            const formData = new FormData();
+
+            formData.append("file", selectedFile);
+
+            const metadata = JSON.stringify({
+              name: category,
+            });
+            formData.append("pinataMetadata", metadata);
+
+            const options = JSON.stringify({
+              cidVersion: 0,
+            });
+            formData.append("pinataOptions", options);
+
+            try {
+                const res = await fileUpload(
+                  formData,
+                  name,
+                  title,
+                  description,
+                  external_url
+                );
+
+                if (res.success === true) {
+                  try {
+                    await ConnectToMetamask();
+
+                    var metadataURL = res.pinataURL;
+                    console.log(metadataURL);
+                    const provider = new ethers.providers.Web3Provider(window.ethereum);
+                    const signer = provider.getSigner();
+
+                    // Contract call
+                            let contract = new ethers.Contract(contractAddress, abi, signer);
+                  ethers.utils.parseEther(price);
+
+                  // let listingPrice = await contract.getListPrice();
+                  // listingPrice = listingPrice.toString();
+
+                  //actually create the NFT
+                  let tip = { value: ethers.utils.parseEther("0.1") };
+
+                  //  var url1 = "https://gateway.pinata.cloud/ipfs/QmcZKH8Vu5D8CAkANQg5ocHmFsmzDqVQXh2efM6NRcCgoQ"
+                  //var url2 = https://gateway.pinata.cloud/ipfs/QmUSGVFeHLcEp6EFuLT1CSbHGQgRkrdMxciAPghA7nvhyM
+                  // var url3 https://gateway.pinata.cloud/ipfs/QmdCRNUsGERLXBy4mdhQajqVBaR45dCPGP2ihymokWWGkh
+                  var url4 =
+                    "https://gateway.pinata.cloud/ipfs/QmSA2Q9RaqwDf4MWdZ1xNYyn7PFxajpiVWDrhfNCLSZ2zz";
+                  let transaction = await contract.createToken(metadataURL, price, tip);
+
+                  await transaction.wait();
+                  // setStatus("NFT Minted Successfully")
+                  alert("transaction Succesful");
+
+                window.location.replace("/profile");
+              } catch (err) {
+                console.log(err.message);
+              }
+            } else {
+              setStatus("NFT Upload to PINNATA UNSUCCESSFUL");
+            }
+          } catch (err) {
+            console.log(err.message);
+          }
   };
 
   return (
