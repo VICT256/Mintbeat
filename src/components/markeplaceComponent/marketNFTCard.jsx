e3a48701847e906756409d0bc00290bec2a0541d
chizycodes <okwudirejoy@gmail.com>
2022-12-04 13:11:03 +0100

    commit

--- src/components/markeplaceComponent/marketNFTCard.jsx
+++ src/components/markeplaceComponent/marketNFTCard.jsx
@@ -1,34 +1,31 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function MarketNFTCard(nft) {
-  return (
-    <div className="bg-[#000000CC] shadow-lg w-full md:max-w-[466px] rounded-lg">
-      <div className="flex flex-col md:flex-row w-full gap-3 h-full">
-        <img
-          src={nft.image}
-          alt="music cover"
-          className="w-full md:w-[200px] h-[180px] md:h-[207px] rounded-l-lg"
-        />
-        <div className="h-full p-5 justify-between md:p-3 flex flex-col">
-          <p className="flex flex-col">
-            <span className="text-[#FFFFFF] opacity-[0.75] text-[24px] font-bold">
-              {nft.name}
-            </span>
-            <span className="text-[#C4C4C4] text-[16px]">{nft.title}</span>
-            {/* <span className="text-[#FBF1F3] text-[18px]">By Maroon 5</span> */}
-            {/* <span className="text-[#C4C4C4] text-sm">Nov 20 2022</span> */}
-          </p>
-          <div className="flex items-center justify-between mt-5">
-            <p>{nft.tokenID}</p>
-            <button className="btn-primary bg-[#E9003F] px-3 py-2">
-              <span>Buy </span>
-              <span className="text-[#FFF]">{nft.price}</span>
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	console.log(nft.nft, 'nft');
+
+	const [data, setData] = useState(nft.nft.metadata);
+	return (
+		<div className="bg-[#000000CC] shadow-lg w-full md:max-w-[466px] rounded-lg">
+			<div className="flex flex-col md:flex-row w-full gap-3 h-full">
+				<img src={data.url} alt="music cover" className="w-full md:w-[200px] h-[180px] md:h-[207px] rounded-l-lg" />
+				<div className="h-full p-5 justify-between md:p-3 flex flex-col">
+					<p className="flex flex-col">
+						<span className="text-[#FFFFFF] opacity-[0.75] text-[24px] font-bold">{data.description}</span>
+						<span className="text-[#C4C4C4] text-[16px]">{nft.title}</span>
+						<span className="text-[#FBF1F3] text-[18px]">By {data.name_of_artiste}</span>
+						{/* <span className="text-[#C4C4C4] text-sm">Nov 20 2022</span> */}
+					</p>
+					<div className="flex items-center justify-between mt-5">
+						<p>{nft.tokenID}</p>
+						<button className="btn-primary bg-[#E9003F] px-3 py-2">
+							<span>Buy </span>
+							<span className="text-[#FFF]">{data.initial_price_set}</span>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default MarketNFTCard;
--- src/components/markeplaceComponent/nftMart.jsx
+++ src/components/markeplaceComponent/nftMart.jsx
@@ -1,148 +1,117 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { ethers } from "ethers";
-import MarketNFTCard from "./marketNFTCard";
-import {
-  ConnectToMetamask,
-  getWalletConnected,
-} from "../utils/functions/connect";
-import abi from "../../contractAbi.json";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { ethers } from 'ethers';
+import MarketNFTCard from './marketNFTCard';
+import { ConnectToMetamask, getWalletConnected } from '../utils/functions/connect';
+import abi from '../../contractAbi.json';
 
-const contractAddress = "0x07CfE3773b86d15deC743BB7f37d498dF0562F51";
+const contractAddress = '0x07CfE3773b86d15deC743BB7f37d498dF0562F51';
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
-let contract = new ethers.Contract(contractAddress, abi, signer)
+let contract = new ethers.Contract(contractAddress, abi, signer);
 
 export default function MarketPlaceComponent() {
-      const [NFTs, setNFTS] = useState([]);
-      const [loading, setLoading] = useState(false);
+	const [NFTs, setNFTS] = useState([]);
+	const [loading, setLoading] = useState(false);
 
-      async function handleArray(allnfts) {
+	async function handleArray(allnfts) {
+		for (let nft of allnfts) {
+			const tokenURI = await contract.tokenURI(nft.tokenId);
+			let meta = await axios.get(tokenURI);
+			meta = meta.data;
+			console.log('hey');
 
-    for (let nft of allnfts ){
+			let price = ethers.utils.formatUnits(nft.price.toString(), 'ether');
+			let item = {
+				name: meta.name,
+				title: meta.title,
+				description: meta.description,
+				image: meta.url,
+				price: price,
+				tokenId: nft.tokenId.toNumber(),
+				seller: nft.seller,
+				owner: nft.owner,
+			};
+			return item;
+		}
+	}
 
-      const tokenURI = await contract.tokenURI(nft.tokenId);
-      let meta = await axios.get(tokenURI)
-          meta = meta.data;
-            console.log("hey")
-     
-      let price = ethers.utils.formatUnits(nft.price.toString(), 'ether')
-      let item = {
-              name: meta.name,
-              title: meta.title,
-              description: meta.description,
-              image: meta.url,
-              price: price,
-              tokenId: nft.tokenId.toNumber(),
-              seller: nft.seller,
-              owner: nft.owner,
-            };
-      return item;
-    }
-  }
+	const fetchAllNFTs = async () => {
+		setLoading(true);
 
-  
+		try {
+			// await ConnectToMetamask();
+			// await getWalletConnected();
 
-  const fetchAllNFTs = async () => {
-    setLoading(true);
+			//create an NFT Token
+			let allnfts = await contract.getAllNFTs();
+			console.log(allnfts);
+			const res = await handleArray(allnfts);
+			console.log(res, 'red');
+			setNFTS(res);
 
-          try {
-                // await ConnectToMetamask();
-                // await getWalletConnected();
+			setLoading(false);
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-                //create an NFT Token
-                let allnfts = await contract.getAllNFTs();
-                console.log(allnfts);
-                const res = await handleArray(allnfts)
-                  setNFTS(res);
-                  console.log(NFTs);
-                  setLoading(false);
+	const fetchNFTsForCollection = async () => {
+		let nfts;
+		const baseURL = 'https://polygon-mumbai.g.alchemy.com/v2/K9IsbfM7Z0jHrR5VTyg0rOsu0ghafL9D/getNFTsForCollection/';
+		const requestOptions = {
+			method: 'GET',
+		};
+		console.log('fetching NFTs for Colection');
+		const fetchURL = `${baseURL}?contractAddress=${contractAddress}&withMetadata=${'true'}`;
+		nfts = await fetch(fetchURL, requestOptions).then((data) => data.json());
+		console.log('nftsss', nfts);
+		setNFTS(nfts.nfts);
+	};
 
-                  } catch (err) {
-                    console.log(err);
-                  }
-    };
-       
-    const fetchNFTsForCollection = async () =>{
-      let nfts;
-      const baseURL = "https://polygon-mumbai.g.alchemy.com/v2/K9IsbfM7Z0jHrR5VTyg0rOsu0ghafL9D/getNFTsForCollection/"
-      const requestOptions = {
-        method : "GET"
-      }
-        console.log("fetching NFTs for Colection");
-        const fetchURL= `${baseURL}?contractAddress=${contractAddress}&withMetadata=${"true"}`
-        nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
-        console.log("nfts: ", nfts) 
-        setNFTS(nfts.nfts); 
-   }
+	//  this is the sample of how you can retrieve the data fro a Card Using fetchNFTSForCollection
+	//  <img  src={prop.nft.media[0].gateway} style={{height:400, width:400}} alt=""/>
+	//  </div>
+	//  <h3>{prop.nft.title}</h3>
+	//  <p>id: {prop.nft.id.tokenId}</p>
+	//  <p>{prop.nft.contract.address}</p>
+	//  <p>{prop.nft.description}</p>
 
-         //  this is the sample of how you can retrieve the data fro a Card Using fetchNFTSForCollection
-        //  <img  src={prop.nft.media[0].gateway} style={{height:400, width:400}} alt=""/>
-        //  </div>  
-        //  <h3>{prop.nft.title}</h3>
-        //  <p>id: {prop.nft.id.tokenId}</p>
-        //  <p>{prop.nft.contract.address}</p>
-        //  <p>{prop.nft.description}</p>
-    
+	useEffect(() => {
+		// fetchAllNFTs();
+		fetchNFTsForCollection();
+	}, []);
 
-  useEffect(() => {
-    // fetchAllNFTs();
-    fetchNFTsForCollection()
-  }, []);
+	return (
+		<div className="mt-[32px] w-full">
+			{/* {NFTs.length > 0 &&  NFTs.map((nft, index)=> <NFTcard key={index} nft={nft} />)} */}
+			{/* <button style={{color: "white"}}  onClick={fetchAllNFTs}>fetch</button> */}
 
-  return (
-    <div className="mt-[32px] w-full">
-      {/* {NFTs.length > 0 &&  NFTs.map((nft, index)=> <NFTcard key={index} nft={nft} />)} */}
-      {/* <button style={{color: "white"}}  onClick={fetchAllNFTs}>fetch</button> */}
+			<div className="w-full mb-10">
+				<div className="mb-[25px]">
+					<h1 className="text-[2rem] text-[#ffffff] font-bold">New Beats</h1>
+				</div>
 
-      <div className="w-full mb-10">
-        <div className="mb-[25px]">
-          <h1 className="text-[2rem] text-[#ffffff] font-bold">New Beats</h1>
-        </div>
-        <h1>{NFTs.description}</h1>
-
-        {NFTs?.map((nft, index) => {
-          <div key={index} className="w-full md:w-auto">
-            <MarketNFTCard nft={nft} />
-          </div>
-        })}
-
-        {NFTs.map((nft, i) => (
-          <div
-            key={i}
-            className="col29 "
-          >
-            <div className="marketplace_nft_text">
-              <div>
-                <p className="text-gray-400">
-                  {" "}
-                  Description : {nft.description}
-                </p>
-              </div>
-            </div>
-          </div>
-        ))}
-
-        <div className="flex flex-wrap gap-5 items-center w-full">
-          {loading ? (
-            "Loading...."
-          ) : (
-            <>
-              {NFTs.length > 0 ? (
-                NFTs?.map((nft, index) => {
-                  <div key={index} className="w-full md:w-auto">
-                    <MarketNFTCard nft={nft} />
-                  </div>;
-                })
-              ) : (
-                <div>No NFT to display</div>
-              )}
-            </>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+				<div className="flex flex-wrap gap-5 items-center w-full">
+					{loading ? (
+						'Loading....'
+					) : (
+						<>
+							{NFTs.length > 0 ? (
+								NFTs?.map((nft, index) => {
+									return (
+										<div key={index} className="w-full md:w-auto">
+											<MarketNFTCard nft={nft} />
+										</div>
+									);
+								})
+							) : (
+								<div>No NFT to display</div>
+							)}
+						</>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 }
-
-
