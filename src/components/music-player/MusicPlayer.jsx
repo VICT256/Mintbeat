ca88fc3d9d2372095d7d50ab223fdc01ff901543
Armstrong Olawande <theonrex1@gmail.com>
2022-12-04 19:14:46 +0100

    music player update

--- src/components/music-player/MusicPlayer.jsx
+++ src/components/music-player/MusicPlayer.jsx
@@ -1,38 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import ReactJkMusicPlayer from 'react-jinke-music-player';
-import 'react-jinke-music-player/assets/index.css';
-import { playerOptions } from '../../utils';
+import React, { useState, useEffect } from "react";
+import ReactJkMusicPlayer from "react-jinke-music-player";
+import "react-jinke-music-player/assets/index.css";
+import { playerOptions } from "../../utils";
 
 const audioList = [
-	{
-		name: 'test',
-		singer: 'Chizycodes',
-		cover: '/images/3.png',
-		musicSrc: '/audio/test.mp3',
-	},
-	{
-		name: 'Bedtime Stories',
-		singer: 'Jay Chou',
-		cover: 'http://res.cloudinary.com/alick/image/upload/v1502375978/bedtime_stories_bywggz.jpg',
-		musicSrc: 'http://res.cloudinary.com/alick/video/upload/v1502375674/Bedtime_Stories.mp3',
-	},
-	{
-		name: 'Dorost Nemisham',
-		singer: 'Sirvan Khosravi',
-		cover: 'https://res.cloudinary.com/ehsanahmadi/image/upload/v1573758778/Sirvan-Khosravi-Dorost-Nemisham_glicks.jpg',
-		musicSrc:
-			'https://res.cloudinary.com/ehsanahmadi/video/upload/v1573550770/Sirvan-Khosravi-Dorost-Nemisham-128_kb8urq.mp3',
-	},
+  {
+    name: "test",
+    singer: "Chizycodes",
+    cover: "/images/3.png",
+    musicSrc: "/audio/test.mp3",
+  },
+  {
+    name: "Bedtime Stories",
+    singer: "Jay Chou",
+    cover:
+      "http://res.cloudinary.com/alick/image/upload/v1502375978/bedtime_stories_bywggz.jpg",
+    musicSrc:
+      "http://res.cloudinary.com/alick/video/upload/v1502375674/Bedtime_Stories.mp3",
+  },
+  {
+    name: "Dorost Nemisham",
+    singer: "Sirvan Khosravi",
+    cover:
+      "https://res.cloudinary.com/ehsanahmadi/image/upload/v1573758778/Sirvan-Khosravi-Dorost-Nemisham_glicks.jpg",
+    musicSrc:
+      "https://res.cloudinary.com/ehsanahmadi/video/upload/v1573550770/Sirvan-Khosravi-Dorost-Nemisham-128_kb8urq.mp3",
+  },
 ];
 
 const MusicPlayer = () => {
-	const [params, setParams] = useState({ ...playerOptions, audioLists: audioList });
+  const [params, setParams] = useState({
+    ...playerOptions,
+    audioLists: audioList,
+  });
 
-	return (
-		<div className="bg-[#141414]">
-			<ReactJkMusicPlayer {...params} />
-		</div>
-	);
+  return (
+    <div className="bg-[#141414]">
+      <ReactJkMusicPlayer {...params} />
+    </div>
+  );
 };
 
 export default MusicPlayer;
